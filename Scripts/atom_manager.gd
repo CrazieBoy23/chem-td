df3e107d0f7627c212755e9e6c01969841739f42
CrazieBoy23 <andrei.mares2008@gmail.com>
2025-06-01 16:07:36 +0300

    morse-potential

--- Scripts/atom_manager.gd
+++ Scripts/atom_manager.gd
@@ -2,30 +2,42 @@ extends Node2D
 class_name AtomManager
 
 @export var start_atom: PackedScene
+var atoms: Array[Atom]
+
+# Morse potential parameters
+var D_e = 350       # Potential well depth
+var a = 0.05      # Width of the well
+var r_e = 100.0     # Equilibrium bond length
 
 func _ready():
 	spawn_start()
 
 func spawn_start():
-	var start1 = start_atom.instantiate() as RigidBody2D
-	var start2 = start_atom.instantiate() as RigidBody2D
+	var start1 = start_atom.instantiate() as Atom
+	var start2 = start_atom.instantiate() as Atom
 
 	start1.position = Vector2(350, 200)
-	start2.position = Vector2(400, 200)
+	start2.position = Vector2(470, 200)
 
-	start1.freeze = true
+	atoms.append(start1)
+	atoms.append(start2)
 
 	add_child(start1)
 	add_child(start2)
 
-	start2.apply_impulse(Vector2.ZERO, Vector2.RIGHT * 50) # Apply impulse at center
+func _physics_process(delta):
+	#Morse potential and derivative of the force calculations
+	var pos1 = atoms[0].position
+	var pos2 = atoms[1].position
+	var r_vec = pos2 - pos1
+	var r = r_vec.length()
+	var direction = r_vec.normalized()
 
-	var joint = DampedSpringJoint2D.new()
-	joint.node_a = start1.get_path()
-	joint.node_b = start2.get_path()
-	joint.length = start1.position.distance_to(start2.position) # natural length
-	joint.stiffness = 1.0
-	joint.damping = 1
-	joint.position = (start1.position + start2.position) / 2
+	var exp_term = exp(-a * (r - r_e))
+	var force_mag = 2 * a * D_e * (1 - exp_term) * exp_term
 
-	add_child(joint)
+	var force = direction * force_mag
+	print(exp_term, "  ", force_mag, force)
+
+	atoms[0].apply_central_force(force)
+	atoms[1].apply_central_force(-force)
--- Scripts/atom_spring.gd
+++
@@ -1,5 +0,0 @@
-extends DampedSpringJoint2D
-class_name AtomSpring
-
-func _physics_process(delta):
-	pass
