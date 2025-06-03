e73affcab5b93713b557f1ab2e9856ffaa235ea9
vVic1029v <victoryvictor11029@gmail.com>
2025-06-03 18:59:04 +0300

    good fucking force function

--- Scripts/atom.gd
+++ Scripts/atom.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 class_name Atom
 
-@export var nr_connections: int
-@export var D_e: int       # Potential well depth
-@export var a: float      # Width of the well
-@export var r_e: float   # Equilibrium bond length
+@export var nr_connections: int = 4
+@export var D_e: int = 350       # Potential well depth
+@export var a: float = 0.05      # Width of the well
+@export var r_e: float = 100.0   # Equilibrium bond length
+@export var extended_modifier: float = 0.2
+var id: int
 
 func get_well_width():
 	return a
--- Scripts/atom_manager.gd
+++ Scripts/atom_manager.gd
@@ -2,8 +2,8 @@ extends Node2D
 class_name AtomManager
 
 @export var start_atom: PackedScene
-var atoms: Array[Atom]
-var bonds: Dictionary = {}
+#var atoms: Array[Atom]
+var bonds: Dictionary[Atom, Array] = {}
 
 # Morse potential parameters
 
@@ -22,14 +22,17 @@ func spawn_start():
 	var start1 = start_atom.instantiate() as Atom
 	var start2 = start_atom.instantiate() as Atom
 	var start3 = start_atom.instantiate() as Atom
+	start1.id = 1
+	start2.id = 2
+	start3.id = 3
 
 	start1.position = Vector2(350, 200)
 	start2.position = Vector2(470, 200)
 	start3.position = Vector2(410, 270)
 
-	atoms.append(start1)
-	atoms.append(start2)
-	atoms.append(start3)
+	#atoms.append(start1)
+	#atoms.append(start2)
+	#atoms.append(start3)
 
 	add_child(start1)
 	add_child(start2)
@@ -38,6 +41,8 @@ func spawn_start():
 	add_bond(start1, start2)
 	add_bond(start1, start3)
 	add_bond(start2, start3)
+	
+	start2.apply_central_impulse(Vector2(500, 0))
 
 func _physics_process(_delta):
 	var processed_pairs := {}
@@ -55,15 +60,37 @@ func _physics_process(_delta):
 
 			# Apply equal and opposite forces to both atoms
 			apply_bond_force(atom_a, atom_b)
+			if atom_a.id == 0:
+				atom_a.apply_central_force(Vector2(100, 0))
 	print("-------")
 
 func apply_bond_force(atom_a: Atom, atom_b: Atom):
-		#Morse potential and derivative of the force calculations
+	#Morse potential and derivative of the force calculations
+	#var pos1 = atom_a.position
+	#var pos2 = atom_b.position
+	#var r_vec = pos2 - pos1
+	#var r = r_vec.length()
+	#var direction = r_vec.normalized()
+	#var a = atom_a.get_well_width()
+	#var D_e = atom_a.get_potential_well_depth()
+	#var r_e = atom_a.get_equilibrum_bond_length()
+#
+	#var exp_term = exp(-a * (r - r_e))
+	#var force_mag = 2 * a * D_e * (1 - exp_term) * exp_term # + D_e * a * (r - r_e)
+#
+	#var force = direction * force_mag
+#
+	#atom_b.apply_central_force(-force)
+	#atom_a.apply_central_force(force)
+	#print(-force)
+	
+	# ANOTHER VER MODIFIED
 	var pos1 = atom_a.position
 	var pos2 = atom_b.position
 	var r_vec = pos2 - pos1
 	var r = r_vec.length()
 	var direction = r_vec.normalized()
+
 	var a = atom_a.get_well_width()
 	var D_e = atom_a.get_potential_well_depth()
 	var r_e = atom_a.get_equilibrum_bond_length()
@@ -71,8 +98,18 @@ func apply_bond_force(atom_a: Atom, atom_b: Atom):
 	var exp_term = exp(-a * (r - r_e))
 	var force_mag = 2 * a * D_e * (1 - exp_term) * exp_term
 
+	# Slight boost at long distances to prevent atoms from drifting too far
+	if r > r_e * 1.5:
+		force_mag += (r - r_e) * (atom_a.extended_modifier + atom_b.extended_modifier)*0.5
+	
 	var force = direction * force_mag
 
 	atom_b.apply_central_force(-force)
 	atom_a.apply_central_force(force)
-	print(-force)
+	
+	const damping_coefficient = 0.5
+	var relative_velocity = atom_b.linear_velocity - atom_a.linear_velocity
+	var damping_force = direction * relative_velocity.dot(direction) * damping_coefficient
+
+	atom_a.apply_central_force(damping_force)
+	atom_b.apply_central_force(-damping_force)
